d5bdc2157289b3a6984bf3474ff71dc832964e8b
Fahmi Fisal <ryuzaki_l@y7mail.com>
2024-03-19 11:42:39 +0800

    update
    fix and update

--- Untitled-1.sql
+++
@@ -1,8 +0,0 @@
-SELECT "ufnFISCreateProdNo" (81,NULL,'03','03','003','13',NULL,'82','SDVIADMIN','8104','1',NULL,NULL,'0006',NULL,'82','I') AS GroupCommodityItemId;
-
-SELECT "ufnFISCreateProdNo" (83,NULL,'20','15','001','08',NULL,'01','SDVIADMIN','NULL','1','1',NULL,'1','55','69','I') AS GroupCommodityItemId;
-
-SELECT "ufnFISCreateProdNo" (83,NULL,'20','15','001','08',NULL,'01','SDVIADMIN',NULL,'1','1','55','0006',NULL,'69','I') AS GroupCommodityItemId;
-
-
-SELECT "ufnFISCreateProdNo" (83,NULL,'20','15','001','08',NULL,'01','SDVIADMIN','NULL','1',NULL,NULL,'1','55','69','I') AS GroupCommodityItemId;
